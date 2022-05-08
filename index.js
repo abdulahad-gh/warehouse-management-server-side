2f89055f407bfeaeeda3ef5c491cfeed7de18702
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-05-08 19:16:32 +0600

    connect to db

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ app.use(cors())
 app.use(express.json())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.rwauo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('connect to db')
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 
