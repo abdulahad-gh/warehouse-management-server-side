7bad167e38d1f499bcd81cbe507d3b2652b4c8a1
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-05-08 20:31:01 +0600

    create api for find single element from db

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
@@ -10,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.rwauo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -24,6 +25,14 @@ async function run() {
             const cursor = inventoriesCollection.find(query)
             const inventories = await cursor.toArray()
             res.send(inventories)
+        })
+
+        app.get('/inventories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventorie = await inventoriesCollection.findOne(query);
+            res.send(inventorie)
+
         })
 
     }
